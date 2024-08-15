b43bba489a88054f1782c767614207e5a135ca51
Hector Palop <hectorpalop@gmail.com>
2024-08-15 03:06:55 +0200

    missing from the last commit

--- Stochastic_Trinkets/trinket_stats_generator.py
+++ Stochastic_Trinkets/trinket_stats_generator.py
@@ -1,8 +1,30 @@
 import json
 import os
 import ollama
+import ast
 
-
+def parse_effects(LLM_effects, vanilla_stats):
+    try:
+        parsed_effects = LLM_effects.strip('[]').split(', ')
+    except:
+        try:
+            preparsed_effects = ast.literal_eval(LLM_effects)
+            parsed_effects = f"[{', '.join(preparsed_effects)}]"
+        except:
+            print('incorrect python format')
+            return False
+    if not all(effect[1:].strip() in vanilla_stats for effect in parsed_effects):
+        print('effect not recognized:')
+        for effect in parsed_effects:
+            print (effect[1:].strip())
+        return False
+    else:
+        result_dict = {}
+        for item in parsed_effects:
+            sign = item[0]
+            key = item[1:].strip()
+            result_dict[key] = sign
+        return result_dict
 
 
 def extract_names(json_file_path):
@@ -26,7 +48,7 @@ def make_trinket_stats_system_prompt(model, temp, stat_list, trinket_name):
         "You should choose stats that are representative of the trinket's title, which is: " + trinket_name + ". "
         "Choose a minimum of 1 and a maximum of 5 stats. Balance positive with negative effects."
         "Precede each stat with either a + or a - depending on whether the effect is positive or negative."
-        "Each stat must be one of the following list:"
+        "EACH STAT SHOULD BE ONE OF THE FOLLOWING LIST (WRITE THEM EXACTLY AS THEY APPEAR HERE):"
     )
     trinket_list_text = " ".join(stat_list)
 
@@ -44,7 +66,7 @@ if __name__ == "__main__":
     print(f"vanilla_stats [{len(vanilla_stats)}] : {vanilla_stats}")
 
     trinket_name_placeholder = "Moonwhisper's Tear"
-    trinket_namer_modelfile = make_trinket_stats_system_prompt(model='llama3:8b', temp='0.9', stat_list=vanilla_stats, trinket_name=trinket_name_placeholder)
+    trinket_namer_modelfile = make_trinket_stats_system_prompt(model='llama3:8b', temp='0.7', stat_list=vanilla_stats, trinket_name=trinket_name_placeholder)
     # print (trinket_namer_modelfile)
 
     ollama.create(model='trinket_namer', modelfile=trinket_namer_modelfile)
@@ -53,10 +75,11 @@ if __name__ == "__main__":
     response = ollama.chat(model='trinket_namer', messages=[
     {
         'role': 'user',
-        'content': 'Please suggest a list of trinket stats. Answer only with a list of these stats in a python list.',
+        'content': 'Please suggest a list of trinket stats. Answer ONLY with a python list with these stats and NOTHING ELSE.',
     },
     ])
     print(response['message']['content'])
+    print(parse_effects(response['message']['content'], vanilla_stats))
 
 
     
