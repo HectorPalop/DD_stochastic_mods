f9ef2620aaccc7e92923284ec59f329212d54771
Hector Palop <hectorpalop@gmail.com>
2024-08-18 02:46:28 +0200

    updated buff analyzer

--- Stochastic_Trinkets/buff_analyzer.py
+++ Stochastic_Trinkets/buff_analyzer.py
@@ -1,31 +1,31 @@
 import json
 import os
 
-def extract_names(json_data):
+def extract_entries_with_param(json_data, param):
     names = {}
     
-    for effect in json_data['effects']:
-        name = effect.get('name')
+    for effect in json_data['buffs']:
+        name = effect.get(param)
         if name:
             names[name] = name
     
     return names
 
-def main():
+if __name__ == "__main__":
     # Get the directory of the current script
     script_dir = os.path.dirname(os.path.abspath(__file__))
     # Path to the JSON file
-    json_file_path = os.path.join(script_dir, 'trinket_effects.json')
+    json_file_path = os.path.join(script_dir, 'vanilla_trinket_buffs.json')
     
     # Load JSON data from file
     with open(json_file_path, 'r') as file:
         data = json.load(file)
 
     # Extract names
-    names = extract_names(data)
+    parameter_to_extract = 'stat_type'
+    stat_types = extract_entries_with_param(data, parameter_to_extract)
+    print ("STAT TYPES: ", stat_types.values())
 
-    # Print the result
-    print (names.values())
-
-if __name__ == "__main__":
-    main()
+    parameter_to_extract = 'stat_sub_type'
+    stat_sub_types = extract_entries_with_param(data, parameter_to_extract)
+    print ("STAT SUBTYPES: ", stat_sub_types.values())
